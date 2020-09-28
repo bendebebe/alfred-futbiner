f6ed9c83043ee8471cabf4977bfb0c25b6773348
Ben Debebe <bendebebe@gmail.com>
2020-09-28 03:18:36 -0400

    Bug fixes

--- index.js
+++ index.js
@@ -3,50 +3,53 @@ const alfy = require('alfy');
 const axios = require('axios');
 
 (async () => {
-	const data = await alfy.fetch('https://www.futbin.com/search', {
+	let data = await alfy.fetch('https://www.futbin.com/search', {
 		query: {
             year: 21,
             extra: 1,
             v: 1,
 			term: alfy.input
         },
-        json: true,
-        cache: false
-	});
+        json: false
+    });
 
-
-	const items = data.results.map(async player => {
+    let output = []
+	for (const player of JSON.parse(data.replace('\n', ''))) {
         const player_id = player.image.match(/\d*\.png/)[0].split('.')[0];
-        const prices = await axios.get('https://www.futbin.com/21/playerPrices?player=' + player_id)[player_id][prices];
+        let prices = await axios.get('https://www.futbin.com/20/playerPrices?player=' + player_id);
+        prices = prices['data'][player_id]['prices'];
 
-        const title = Buffer.from((await axios.get(player.club_image, {
-            responseType: 'arraybuffer'
-        })).data, 'binary').toString('base64');
+        // let title = Buffer.from((await axios.get(player.club_image, {
+        //     responseType: 'arraybuffer'
+        // })).data, 'binary').toString('base64');
 
-        title += ' ' + Buffer.from((await axios.get(player.nation_image, {
-            responseType: 'arraybuffer'
-        })).data, 'binary').toString('base64');
+        // title += ' ' + Buffer.from((await axios.get(player.nation_image, {
+        //     responseType: 'arraybuffer'
+        // })).data, 'binary').toString('base64');
         
-        title += ' ' + Buffer.from((await axios.get(player.image, {
-            responseType: 'arraybuffer'
-        })).data, 'binary').toString('base64');
+        // title += ' ' + Buffer.from((await axios.get(player.image, {
+        //     responseType: 'arraybuffer'
+        // })).data, 'binary').toString('base64');
 
-        title += ' ' + player.full_name + `(${player.position}) - ${player.rating}`;
+        let title = player.full_name + ` (${player.position}) - ${player.rating}`;
 
-        const subtitle = `XBOX: ${prices['xbox']['LCPrice']} (Updated: ${prices['xbox']['updated']}), `
-        subtitle += `PS: ${prices['ps']['LCPrice']} (Updated: ${prices['ps']['updated']}), `
-        subtitle += `PC: ${prices['pc']['LCPrice']} (Updated: ${prices['pc']['updated']})`
+        // let subtitle = `XBOX: ${prices['xbox']['LCPrice']} (Updated: ${prices['xbox']['updated']}), `
+        // subtitle += `PS: ${prices['ps']['LCPrice']} (Updated: ${prices['ps']['updated']}), `
+        // subtitle += `PC: ${prices['pc']['LCPrice']} (Updated: ${prices['pc']['updated']})`
 
+        let subtitle = `XBOX: ${prices['xbox']['LCPrice']} | `
+        subtitle += `PS: ${prices['ps']['LCPrice']} | `
+        subtitle += `PC: ${prices['pc']['LCPrice']}`
 
-		return {
+		output.push({
             title: title,
             subtitle: subtitle,
-			arg: player_id,
+			arg: 'https://www.futbin.com/21/player/542/' + player_id,
 			icon: {
 				path: 'icon.png' // Hide icon
 			}
-		};
-	});
-
-	alfy.output(items);
+		});
+    };
+    
+	alfy.output(output);
 })();
